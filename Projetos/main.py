9d36fa1f5e61ace78fd777698eec2f4ebdde59ce
AGHeiss <augustoheiss@gmail.com>
2024-04-04 21:03:52 -0300

    Territórios

--- Projetos/main.py
+++ Projetos/main.py
@@ -5,16 +5,6 @@ os.system('cls')
 
 teste_01 = Territorios()
 
-teste_01.designar_territorio('02/09/2023', 1)
-teste_01.designar_territorio('26/09/2023', 1)
-teste_01.designar_territorio('04/10/2023', 1)
-teste_01.designar_territorio('05/10/2023', 1)
-teste_01.designar_territorio('24/10/2023', 1)
-teste_01.designar_territorio('11/11/2023', 1)
-teste_01.designar_territorio('06/12/2023', 1)
-teste_01.designar_territorio('07/12/2023', 1)
-teste_01.designar_territorio('01/01/2024', 1)
-teste_01.designar_territorio('20/01/2024', 1)
 
 
 
